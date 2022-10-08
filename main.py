87f976a656be11684d2eae88ee47566c6c1d5a31
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-08 10:51:09 +0300

    lab 2 done

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 
 #dev
+
+num1=int(input("Enter first num: "))
+print(f"!{num1}=")
+
+sum=1
+for i in range(1,num1+1):
+    if i == num1:
+        print(i, end="")
+    else:
+        print(f"{i} * ", end="")
+    sum*=i
+print(f"= {sum}")
+
