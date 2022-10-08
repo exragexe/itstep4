8a9904298ef8ca7eb06eba3496a45f3a8629a318
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-08 10:29:48 +0300

    lab 1 done

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 
 #dev
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+sum=0
+for i in range(num1,num2+1):
+    print(f"{sum}+{i} = ", end="")
+    sum +=i
+    print(f"{sum}")
+
+print(f"{sum} = sum")
+print(f"{int(sum/num2)} = ariph")
