27be92937ec45799c5ab5a2067bcd2bdf09ecba1
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-08 11:05:33 +0300

    lab 3 done

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 
 #dev
+dovzh= int(input("Enter length of lines: "))
+znak = str(input("Znak: "))
+triger = str(input("horizontal - h, vertical - v: "))
+
+for i in range(1,dovzh+1):
+    if triger =="v":
+      print(znak)
+    elif triger =="h":
+        print(znak, end="")
+    else:
+        raise Exception("Incorect choose of triger")
