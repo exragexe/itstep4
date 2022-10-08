ea6c2ca8c0f9862514edbfc10f2d605a3f910c53
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-08 12:40:36 +0300

    hm 4 done

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 
 #dev
+try:
+
+    while True:
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+
+        if num1 == 7 or num2 ==7:
+            print("Good bye!")
+            break
+        elif num1 > num2:
+            print(f"{num1} - max, {num2} - min, {num1}+ {num2} = {int(num1+num2)}")
+        elif num1 < num2:
+            print(f"{num2} - max, {num1} - min, {num2}+ {num1} = {int(num2+num1)}")
+
+
+except Exception as ex:
+    print(ex)
