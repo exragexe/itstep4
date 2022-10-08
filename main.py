06ecb5721d80b78d9753f69c3b59d32cbaae6b31
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-08 12:26:44 +0300

    hm 3 done

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 
 #dev
+try:
+
+
+    while True:
+        num1 = int(input("Enter number: "))
+
+        if num1 == 7:
+            print("Good bye!")
+            break
+
+
+        elif num1 > 0:
+            print("Number is positive")
+        elif num1 < 0:
+            print("Number is negative")
+        elif num1 == 0:
+            print("Number is equal to zero")
+
+
+except Exception as ex:
+    print(ex)
