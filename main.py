52d9d7641bb5209c4bdbca87a085ad87c58e1e05
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-08 11:10:35 +0300

    lab 4 done

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 
 #dev
+try:
+    num1=int(input("Enter length of lines: "))
+    znak = str(input("Znak: "))
+    for i in range(1,num1+1):
+        print(znak, end="")
+except Exception as ex:
+    print("not correct num1 or znak")
